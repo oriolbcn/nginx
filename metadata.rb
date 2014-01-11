c6f06abde4a9d4c4d04e17fa183cb9b5bda95dd0
Oriol Collell <oriol.collell.bcn@gmail.com>
2014-01-11 17:03:02 +0100

    updated some stuff

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name              "nginx"
 maintainer        "Oriol Collell"
 maintainer_email  "ocollell@dineyo.com"
 description       "Installs the nginx web server from the nginx ppa"
-version           "0.0.4"
+version           "0.0.5"
 
 recipe "nginx", "nginx server"
 
