be20002b0e7fb916b335e15dc1ec8b5676c59fd1
Oriol Collell <oriol.collell.bcn@gmail.com>
2014-11-10 18:03:10 +0100

    added config options for forward secrecy

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name              "nginx"
 maintainer        "Oriol Collell"
 maintainer_email  "ocollell@dineyo.com"
 description       "Installs the nginx web server from the nginx ppa"
-version           "0.0.7"
+version           "0.0.8"
 
 recipe "nginx", "nginx server"
 
